dc3b14ccc719588385b4ad601ac85704480aef79
shirne <shirne@126.com>
2022-08-14 09:12:34 +0800

    fix comment

--- lib/src/code_page.dart
+++ lib/src/code_page.dart
@@ -321,7 +321,7 @@ const _controls = "\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007"
     "\u0012\u0013\u0014\u0015\u0016\u0017\u0018"
     "\u0019\u001a\u001b\u001c\u001d\u001e\u001f";
 
-/// ASCII characters without control characters. Shared by many code pages.
+/// ASCII characters with control characters. Shared by many code pages.
 const _ascii = "$_controls"
     r""" !"#$%&'()*+,-./0123456789:;<=>?"""
     r"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_"
